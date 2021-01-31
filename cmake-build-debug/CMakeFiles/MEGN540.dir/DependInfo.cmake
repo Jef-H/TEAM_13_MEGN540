edddfb5e0b25c9d98379daeb6e4d62a80f5dcc77
Jef Hinton <Jhinton@us.ci.org>
2021-01-31 15:13:22 -0700

    add class repo

---
+++ cmake-build-debug/CMakeFiles/MEGN540.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Buffalo/CLionProjects/MEGN540/Lab_0/Lab0.c" "/Users/Buffalo/CLionProjects/MEGN540/cmake-build-debug/CMakeFiles/MEGN540.dir/Lab_0/Lab0.c.o"
+  "/Users/Buffalo/CLionProjects/MEGN540/Lab_1/Lab1.c" "/Users/Buffalo/CLionProjects/MEGN540/cmake-build-debug/CMakeFiles/MEGN540.dir/Lab_1/Lab1.c.o"
+  "/Users/Buffalo/CLionProjects/MEGN540/Ring_Buffer/Ring_Buffer.c" "/Users/Buffalo/CLionProjects/MEGN540/cmake-build-debug/CMakeFiles/MEGN540.dir/Ring_Buffer/Ring_Buffer.c.o"
+  "/Users/Buffalo/CLionProjects/MEGN540/Ring_Buffer/main.c" "/Users/Buffalo/CLionProjects/MEGN540/cmake-build-debug/CMakeFiles/MEGN540.dir/Ring_Buffer/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../Lab_0"
+  "../Lab_1"
+  "../Ring_Buffer"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MEGN540.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MEGN540.dir/Lab_0/Lab0.c.o"
+  "CMakeFiles/MEGN540.dir/Lab_1/Lab1.c.o"
+  "CMakeFiles/MEGN540.dir/Ring_Buffer/Ring_Buffer.c.o"
+  "CMakeFiles/MEGN540.dir/Ring_Buffer/main.c.o"
+  "MEGN540"
+  "MEGN540.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MEGN540.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
