c41922064dba7d95d4b7b55e21a8b2eed8a232f5
Jef Hinton <Jhinton@us.ci.org>
2021-02-08 22:26:42 -0700

    usb_msg_read_into

--- class_repo/c_lib/SerialIO.c
+++ class_repo/c_lib/SerialIO.c
@@ -479,7 +479,7 @@ bool usb_msg_read_into(void* p_obj, uint8_t data_len) {
     uint8_t receive_len = usb_msg_length();
     if (receive_len >= data_len) {
         for (int i = 0; i < data_len; i++) {
-            *p_obj[i] = usb_msg_get();
+            *p_obj[i]= usb_msg_get();
             return true; }
         } else {
             return false;
