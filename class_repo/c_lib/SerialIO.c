64fa62276664e5a16553b463a1615eba1f2351cb
Jef Hinton <Jhinton@us.ci.org>
2021-02-10 23:26:52 -0700

    add 2 pop

--- class_repo/c_lib/SerialIO.c
+++ class_repo/c_lib/SerialIO.c
@@ -221,7 +221,7 @@ void USB_Echo_Task(void) {
     /* Check to see if any data has been received */
     if (Endpoint_IsOUTReceived()) {
         /* Create a temp buffer big enough to hold the incoming endpoint packet */
-        uint8_t Buffer[Endpoint_BytesInEndpoint()+1];
+        uint8_t Buffer[Endpoint_BytesInEndpoint()];
 
         /* Remember how large the incoming packet is */
         uint16_t DataLength = Endpoint_BytesInEndpoint()+1;
