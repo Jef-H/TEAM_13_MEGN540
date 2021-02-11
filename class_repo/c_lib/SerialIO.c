c1947cffbbc9ce3a29eeece98c0ac033e56a551e
Jef Hinton <Jhinton@us.ci.org>
2021-02-10 19:25:00 -0700

    &buffer change

--- class_repo/c_lib/SerialIO.c
+++ class_repo/c_lib/SerialIO.c
@@ -229,10 +229,10 @@ void USB_Echo_Task(void)
 
 		/* Read in the incoming packet into the buffer */
 		//Endpoint_Read_Stream_LE(&Buffer, DataLength, NULL);
-		&Buffer = Endpoint_Read_8();
+		//&Buffer = Endpoint_Read_8();
 
 		// add to buffer.
-        rb_push_back_C(&_usb_receive_buffer, Buffer);
+        rb_push_back_C(&_usb_receive_buffer, Endpoint_Read_8());
 
 		/* Finalize the stream transfer to send the last packet */
 		Endpoint_ClearOUT();
