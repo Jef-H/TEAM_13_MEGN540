f25e38a4305637055a4cfdd27677c6e6f22aece4
Jef Hinton <Jhinton@us.ci.org>
2021-02-21 09:57:00 -0700

    Revert "send c"
    This reverts commit cc04507f7a524f3ae21ee7d7e9e468bb10b2f6be.

--- class_repo/Lab2-Timing/Lab2-Timers.c
+++ class_repo/Lab2-Timing/Lab2-Timers.c
@@ -50,7 +50,6 @@ int main(void) {
 
         GetTime();
         USB_USBTask();
-        usb_send_byte('c');
         if (MSG_FLAG_Execute(&mf_restart))// TODO add desired timer
         {
             //re initialzie your stuff...
